693440bc8a89c5390cc923382d58435236842b71
Dilkash <dilkashpeshimam@gmail.com>
2022-10-18 18:22:46 +0530

    handling select value

--- src/components/Expense/Expenses.js
+++ src/components/Expense/Expenses.js
@@ -1,10 +1,18 @@
-import React from 'react'
+import React, {useState}from 'react'
+import ExpenseFilter from './ExpenseFilter'
 import ExpenseItem from './ExpenseItem'
 
 const Expenses = ({ expenseName, expenseDate, expenseAmount }) => {
+  const [filterYear,setFilterYear]=useState('2020')
+
+  const handleFilter=(selectedYear)=>{
+    setFilterYear(selectedYear)
+    console.log(filterYear)
+  }
   return (
     <div>
-        <ExpenseItem expenseName={expenseName} expenseDate={expenseDate} expenseAmount={expenseAmount}/>
+      <ExpenseFilter handleFilter={handleFilter} filterYear={filterYear} />
+        <ExpenseItem  expenseName={expenseName} expenseDate={expenseDate} expenseAmount={expenseAmount}/>
     </div>
   )
 }
